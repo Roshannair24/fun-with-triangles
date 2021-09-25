20c68df0684f5c6f063e02f1436a187499fa51a3
Roshannair24 <70207465+Roshannair24@users.noreply.github.com>
2021-09-25 14:17:42 +0530

    feat updated

---
+++ Triangle Quiz.js
@@ -0,0 +1,37 @@
+let quizForm = document.querySelector("#quiz-form");
+let quizbuttonprimary = document.querySelector(".button-primary");
+let outputheading = document.querySelector(".output-heading");
+
+let correctAnswers = ["90°", "right angled"];
+
+quizbuttonprimary.addEventListener("click", calculateScore);
+
+function calculateScore() {
+  let score = 0;
+
+  let index = 0;
+
+  const formData = new FormData(quizForm);
+
+  console.log("Form data");
+
+  console.log(formData);
+  for (var entry of formData.entries()) {
+    console.log(entry);
+  }
+
+  for (var value of formData.values()) {
+    console.log(value);
+
+    if (value === correctAnswers[index]) {
+      score = score + 1;
+    }
+
+    index = index + 1;
+  }
+
+  console.log("score:" + score);
+
+
+  outputheading .innerText="Your score: " + score;
+}
---
+++ isTriangle.js
@@ -0,0 +1,40 @@
+let inputs = document.querySelectorAll(".angles");
+let buttonisTriangle = document.querySelector(".button-isTriangle");
+let outputheadingisTriangle = document.querySelector(
+  ".output-heading-isTriangle"
+);
+
+let sum = null;
+let sumOfAngles = null;
+
+function calculateSumOfAngles(angleA, angleB, angleC) {
+  angleA = Number(angleA);
+  angleB = Number(angleB);
+  angleC = Number(angleC);
+
+  sum = angleA + angleB + angleC;
+
+  return sum;
+}
+
+buttonisTriangle.addEventListener("click", isTriangle);
+
+function isTriangle() {
+  sumOfAngles = calculateSumOfAngles(
+    inputs[0].value,
+    inputs[1].value,
+    inputs[2].value
+  );
+
+  console.log("sumOfAngles: " + sumOfAngles);
+
+  if (sumOfAngles === 180) {
+    console.log("The angles form a triangle");
+
+    outputheadingisTriangle.innerText = "The angles form a triangle";
+  } else {
+    console.log("The angles dont form a triangle");
+
+    outputheadingisTriangle.innerText = "The angles dont form a triangle";
+  }
+}
